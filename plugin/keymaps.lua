101b9a2a256548765de5829e7f9cae281b8d25c9
Migara <migara1225@gmail.com>
2024-05-21 23:50:57 +0530

    added options and keymapping

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -2,3 +2,10 @@ local map = vim.keymap
 
 map.set("n", "<leader>nt", "<cmd>tabnew<cr>", { desc = "Open new Tab" })
 map.set("n", "<leader>tr", "<cmd>terminal<cr>", { desc = "Open Terminal" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
--- plugin/options.lua
+++ plugin/options.lua
@@ -7,6 +7,20 @@ opt.hlsearch = true
 opt.wrap = true
 opt.breakindent = true
 opt.shiftwidth = 2
+vim.g.have_nerd_font = true
+vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
