abd667344d88dc9014b87a1724a5d3a3dfba9add
Migara <migara1225@gmail.com>
2024-05-21 23:40:17 +0530

    made highlight when text is yank

--- plugin/options.lua
+++ plugin/options.lua
@@ -2,8 +2,16 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
-opt.mouse = 'a'
+opt.mouse = "a"
 opt.hlsearch = true
 opt.wrap = true
 opt.breakindent = true
 opt.shiftwidth = 2
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("migara-nvim", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
